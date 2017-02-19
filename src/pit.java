ba47375fe70bb4bf993832c0320e4430ee4abae9
FA <fbagmu@gmail.com>
2017-02-19 11:23:06 -0500

    New class

--- src/pit.java
+++ src/pit.java
@@ -2,4 +2,19 @@
  * Created by FA on 2/17/2017.
  */
 public class pit {
+
+    private int stones;
+
+    public pit() {
+        stones =0;
+    }
+
+    public void setStones(int stones) {
+        this.stones = stones;
+    }
+
+    public int getStones() {
+        return stones;
+    }
+
 }
--- src/player.java
+++ src/player.java
@@ -2,4 +2,28 @@
  * Created by FA on 2/17/2017.
  */
 public class player {
+    private String name;
+    private String turn;
+
+    public player() {
+        name=null;
+        turn=null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTurn() {
+        return turn;
+    }
+
+    public void setTurn(String turn) {
+        this.turn = turn;
+    }
+
 }
