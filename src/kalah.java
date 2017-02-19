9b18a5795b43b5aede39ce633b13ffa5182829ea
FA <fbagmu@gmail.com>
2017-02-19 11:28:05 -0500

    New class

---
+++ src/kalah.java
@@ -0,0 +1,19 @@
+/**
+ * Created by FA on 2/19/2017.
+ */
+public class kalah {
+    private int stones;
+
+    public kalah() {
+        stones=0;
+    }
+
+    public int getStones() {
+        return stones;
+    }
+
+    public void setStones(int stones) {
+        this.stones = stones;
+    }
+
+}
