9b909149cd97a7e1ff684465639f38031b536e1f
KrishnaKRGit <krishnakrdms@gmail.com>
2017-02-20 15:27:53 -0500

    added redistribute methods

--- src/player.java
+++ src/player.java
@@ -31,7 +31,21 @@ public class player {
         gain.addStones(end.opposite.takeAllStones());
         return end;
     }
-      
+
+    /*
+    * This method takes a pit(start) as input.
+    * then empties all the pebbles in the pit,
+    * and redistributes them one by one to other pits,
+    * using subsequent next references.*/
+    public void reDistributeCounterclockwise(pit start){
+        int count = start.takeAllStones();           //take all the pebbles in this pit.
+
+        for(int i=0; i< count ; i++){
+            start = start.next;                  //go to the next pit from start and put 1 stone there.
+            start.addStones(1);           //
+        }
+    }
+
 }
 
 
